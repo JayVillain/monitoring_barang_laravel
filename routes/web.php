820bd3a0298a020498a6d54d720aae36d4dfa364
JayVillain <saipi3825@gmail.com>
2026-04-18 17:07:38 +0000

    Apr 19, 2026, 12:07 AM

--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,47 @@ use App\Http\Controllers\AdminAuthController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\TransactionController;
 
-Route::get('/admin/login', [AdminAuthController::class,'showLoginForm'])->name('admin.login');
-Route::post('/admin/login', [AdminAuthController::class,'login'])->name('admin.login.submit');
-Route::get('/admin/logout', [AdminAuthController::class,'logout'])->name('admin.logout');
-
-Route::middleware(['admin.auth'])->group(function(){
-    Route::get('/dashboard', function(){ return view('dashboard'); })->name('dashboard');
-    Route::resource('items', ItemController::class);
-    Route::resource('transactions', TransactionController::class);
-});
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Public Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Routes yang bisa diakses tanpa login
 |
 */
 
+// Halaman utama
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Halaman login admin
+Route::get('/admin/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/admin/login', [AdminAuthController::class, 'login'])->name('admin.login.submit');
+
+// Logout admin
+Route::get('/admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+
+
+/*
+|--------------------------------------------------------------------------
+| Protected Routes (Admin Only)
+|--------------------------------------------------------------------------
+|
+| Routes ini hanya bisa diakses setelah login admin
+|
+*/
+Route::middleware(['admin.auth'])->group(function () {
+
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // CRUD Barang
+    Route::resource('items', ItemController::class);
+
+    // CRUD Transaksi
+    Route::resource('transactions', TransactionController::class);
+
 });
