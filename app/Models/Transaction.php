aae51367a4c7165e5aeb9f0bb65577c9f9cc62c1
JayVillain <saipi3825@gmail.com>
2026-04-18 17:00:46 +0000

    Apr 19, 2026, 12:00 AM

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -1,11 +1,14 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
     use HasFactory;
+    protected $fillable = ['item_id', 'type', 'quantity', 'date'];
+
+    public function item() {
+        return $this->belongsTo(Item::class);
+    }
 }
