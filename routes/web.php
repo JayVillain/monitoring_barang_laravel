59f58c8b16e0fc83cc5c81286ef6ea1fafcceb32
JayVillain <saipi3825@gmail.com>
2026-04-18 17:06:32 +0000

    Apr 19, 2026, 12:06 AM

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminAuthController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\TransactionController;
 
+Route::get('/admin/login', [AdminAuthController::class,'showLoginForm'])->name('admin.login');
+Route::post('/admin/login', [AdminAuthController::class,'login'])->name('admin.login.submit');
+Route::get('/admin/logout', [AdminAuthController::class,'logout'])->name('admin.logout');
+
+Route::middleware(['admin.auth'])->group(function(){
+    Route::get('/dashboard', function(){ return view('dashboard'); })->name('dashboard');
+    Route::resource('items', ItemController::class);
+    Route::resource('transactions', TransactionController::class);
+});
 /*
 |--------------------------------------------------------------------------
 | Web Routes
